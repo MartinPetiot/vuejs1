38ba3037fb388ee18c05d60975f834efc18d5d5d
Martin PETIOT <martin.petiot@Digital-Grenoble.local>
2021-03-11 16:53:10 +0100

    j'ai mal au crane

---
+++ script.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '#app',
+    data: {
+        message: "C'est un peu comme Blade en fait !"
+    }
+})
+new Vue({
+    el: '#limit',
+    data: {
+        input: "Coucou",
+        h2 : "Salopes",
+        checked : false,
+    }
+})
